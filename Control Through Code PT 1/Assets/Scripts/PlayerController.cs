740bc79069bea84d78416769e9e5152f08f8037b
Josh Jackson <gamedevelopmentguidance@gmail.com>
2023-10-19 16:32:33 -0600

    Initial Commit

---
+++ Control Through Code PT 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        float move_amount = Mathf.Clamp01(Mathf.Abs(movement.x) + Mathf.Abs(movement.z));
+
+        animator.SetFloat("MoveAmount", move_amount);
+
+        if(move_amount > 0f)
+        {
+            Quaternion target_rotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, 500 * Time.deltaTime);
+        }
+    }
+}
